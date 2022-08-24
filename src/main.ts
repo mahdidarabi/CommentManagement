416e8ff550907ae7a43139688fe347d9598cab2b
Mahdi Darabi <mahdidarabi7@yahoo.com>
2022-08-24 17:02:59 +0430

    Add class validator

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config: ConfigService = app.get(ConfigService);
   const port = config.get<number>('port');
   const mode = config.get<string>('mode');
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(port, () => {
     console.log(
